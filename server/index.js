8e0127834b0319c6883568c27e9fa60c0fedf324
ashish-um <ashishumrao632005@gmail.com>
2024-07-02 14:32:45 +0530

    vercel server

--- server/index.js
+++ server/index.js
@@ -11,14 +11,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/auth", userRouter);
-app.use("/post", postRouter);
+// app.use("/auth", userRouter);
+// app.use("/post", postRouter);
 
 app.get("/", (req, res) => {
   res.json({ msg: "Site Online" });
 });
 
-mongoose.connect(process.env.MONGO_URI);
+// mongoose.connect(process.env.MONGO_URI);
 
 app.listen(3000, () => {
   console.log("server running on port 3000");
